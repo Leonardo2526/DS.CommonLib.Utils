483f578bcb75ded9d3badbb6582527593bfe70b9
Leonardo2526 <leonardo147wot@gmail.com>
2022-01-18 14:18:48 +0300

    Add project files.

---
+++ DS.System/Location.cs
@@ -0,0 +1,13 @@
+﻿namespace DS.System
+{
+    public struct Location
+    {
+        public readonly int X, Y, Z;
+        public Location(int x, int y, int z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+    }
+}
