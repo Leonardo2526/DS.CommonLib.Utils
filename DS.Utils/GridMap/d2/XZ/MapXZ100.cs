80b246af30c8c158a918333ab7e2b5937a17a7ef
Leonardo2526 <leonardo147wot@gmail.com>
2022-02-21 14:32:07 +0300

    XZ maps added

---
+++ DS.Utils/GridMap/d2/XZ/MapXZ100.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DS.PathSearch.GridMap.d2
+{
+    /// <summary>
+    /// 2-dimensional grid map size 100x100 with both sides stepped walls. Start and goal points by map's center.
+    /// </summary>
+    public class MapXZ100 : IMap
+    {
+        public Location Start { get; set; } = new Location(0, 0, 49);
+        public Location Goal { get; set; } = new Location(99, 0, 49);
+        public int[,,] Matrix { get; set; }
+
+        public MapXZ100()
+        {
+            Matrix = new int[100, 1, 100];
+
+            //Create walls
+            int middleZ = (int)Math.Round((double)(Matrix.GetUpperBound(2) / 2));
+            int smesh = 2;
+
+            for (int y = 0; y <= Matrix.GetUpperBound(1); y++)
+            {
+                for (int z = middleZ - smesh; z <= Matrix.GetUpperBound(2); z++)
+                {
+                    for (int x = 40; x <= 50; x++)
+                        Matrix[x, y, z] = 1;
+                }
+                for (int z = 0; z <= middleZ + 3*smesh; z++)
+                {
+                    for (int x = 60; x <= 70; x++)
+                        Matrix[x, y, z] = 1;
+                }
+
+            }
+
+
+            Matrix[Start.X, Start.Y, Start.Z] = 8;
+            Matrix[Goal.X, Goal.Y, Goal.Z] = 9;
+        }
+
+
+    }
+}
---
+++ DS.Utils/GridMap/d2/XZ/MapXZ20v1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DS.PathSearch.GridMap.d2
+{
+    /// <summary>
+    /// 2-dimensional grid map size 20x20 with 1 wall. Start and goal points by map's center.
+    /// </summary>
+    public class MapXZ20v1 : IMap
+    {
+        public Location Start { get; set; } = new Location(0, 0, 9);
+        public Location Goal { get; set; } = new Location(19, 0, 9);
+        public int[,,] Matrix { get; set; }
+
+        public MapXZ20v1()
+        {
+            Matrix = new int[20, 1, 20]; 
+          
+            for (int y = 0; y <= Matrix.GetUpperBound(1); y++)
+            {
+                for (int z = 7; z <= 15; z++)
+                    for (int x = 7; x <= 10; x++)
+                        Matrix[x, y, z] = 1;
+            }
+            
+            Matrix[Start.X, Start.Y, Start.Z] = 8;
+            Matrix[Goal.X, Goal.Y, Goal.Z] = 9;
+
+        }
+
+
+    }
+}
