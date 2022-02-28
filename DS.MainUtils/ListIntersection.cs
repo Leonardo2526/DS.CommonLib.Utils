40074d011d6708a61a8ab2d7341ffa86f6906fff
Leonardo2526 <leonardo147wot@gmail.com>
2022-02-28 18:32:16 +0300

    ListIntersection added

---
+++ DS.MainUtils/ListIntersection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DS.MainUtils
+{
+    public static class ListIntersection
+    {
+        public static List<int> GetIntersections(List<int> list1, List<int> list2)
+        {
+            var intersecion = list1.Intersect(list2).ToList();
+            return intersecion;
+        }
+
+        public static List<int> GetNoIntersections(List<int> list1, List<int> list2)
+        {
+            var NoIntersections = new List<int>();
+
+            foreach (var one in list1)
+            {
+                if (!list2.Any(two => two == one))
+                {
+                    NoIntersections.Add(one);
+                }
+            }
+            return NoIntersections;
+        }
+    }
+}
