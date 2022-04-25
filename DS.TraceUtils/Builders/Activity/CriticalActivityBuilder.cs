5750e64f5556df7cbc1356a39199f7d3cd4a6a20
Leonardo2526 <leonardo147wot@gmail.com>
2022-04-25 18:37:29 +0300

    AddMessage added

--- DS.TraceUtils/Builders/Activity/CriticalActivityBuilder.cs
+++ DS.TraceUtils/Builders/Activity/CriticalActivityBuilder.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 
 namespace DS.TraceUtils
 {
-    class ErrorActivityBuilder : ActivityBuilder
+    class CriticalActivityBuilder : ActivityBuilder
     {
-        public ErrorActivityBuilder(string message, string traceSourceName) : base(message, traceSourceName)
+        public CriticalActivityBuilder(string message, string traceSourceName) : base(message, traceSourceName)
         { }
 
         public override ActivityBuilder Build()
         {
-            TS.TraceEvent(TraceEventType.Error, 1, Message);
+            TS.TraceEvent(TraceEventType.Critical, 3, Message);
 
             return this;
         }
