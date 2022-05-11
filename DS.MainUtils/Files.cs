016b1a400f6fc4fde80a3c6372b430316a93112f
Leonardo2526 <leonardo147wot@gmail.com>
2022-05-11 17:11:43 +0300

    Check empty files added

---
+++ DS.MainUtils/Files.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.MainUtils
+{
+    public static class Files
+    {
+        /// <summary>
+        /// Check if file is empty;
+        /// </summary>
+        /// <param name="fullPath"></param>
+        /// <returns>Return true if file by fullPath is empty. Return null if it isn't.</returns>
+        public static bool IsFileEmpty(string fullPath)
+        {
+            long fileLength = new FileInfo(fullPath).Length;
+
+            if (fileLength == 0 || (fileLength == 3 && File.ReadAllBytes(fullPath).SequenceEqual(new byte[] { 239, 187, 191 })))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
