eb2ccfa48ae312723dd9cebf039fa61d6a420363
Leonardo2526 <leonardo147wot@gmail.com>
2022-07-28 20:40:16 +0300

    OperationCreator added

---
+++ DS.ClassLib.VarUtils/OperationCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DS.ClassLib.VarUtils
+{
+    public static class OperationCreator
+    {
+        public static string LongOperation(CancellationTokenSource innerTokenSource)
+        {
+            string s = null;
+            for (int i = 0; i < 30000000; i++)
+            {
+                innerTokenSource.Token.ThrowIfCancellationRequested();
+                s = i.ToString();
+            }
+
+            return s;
+        }
+    }
+}
