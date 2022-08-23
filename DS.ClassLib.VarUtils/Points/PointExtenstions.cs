6e7f5a33b050b367e71df2c299d4c84696488f78
Leonardo2526 <leonardo147wot@gmail.com>
2022-08-23 15:01:15 +0300

    points utils renamed

---
+++ DS.ClassLib.VarUtils/Points/PointExtenstions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.ClassLib.VarUtils.Points
+{
+    public static class PointExtenstions
+    {
+        public static double DistanceTo(this PointModel basePoint, PointModel point)
+        {
+            double xDist = Math.Pow((basePoint.X - point.X) ,2);
+            double yDist = Math.Pow((basePoint.Y - point.Y) ,2);
+            double zDist = Math.Pow((basePoint.Z - point.Z) ,2);
+
+            return Math.Sqrt(xDist + yDist + zDist);
+        }      
+
+    }
+}
--- DS.ClassLib.VarUtils/Points/PointUtils.cs
+++ DS.ClassLib.VarUtils/Points/PointUtils.cs
@@ -6,15 +6,5 @@ using System.Threading.Tasks;
 
 namespace DS.ClassLib.VarUtils.Points
 {
-    public static class PointExtenstions
-    {
-        public static double DistanceTo(this PointModel basePoint, PointModel point)
-        {
-            double xDist = Math.Pow((basePoint.X - point.X) ,2);
-            double yDist = Math.Pow((basePoint.Y - point.Y) ,2);
-            double zDist = Math.Pow((basePoint.Z - point.Z) ,2);
-
-            return Math.Sqrt(xDist + yDist + zDist);
-        }
-    }
+   
 }
