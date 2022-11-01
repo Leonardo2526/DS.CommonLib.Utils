03b697375e86a8b01d40deb43d73dc46fa778570
Данил Сараев <leonardo147wot@gmail.com>
2022-11-01 15:45:18 +0300

    TaskEvents added

---
+++ DS.ClassLib.VarUtils/TaskEvents/ITaskEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace DS.ClassLib.VarUtils
+{
+    /// <summary>
+    /// Interface to create task events.
+    /// </summary>
+    public interface ITaskEvent
+    {
+        /// <summary>
+        /// Create a new task event.
+        /// </summary>
+        /// <returns></returns>
+        public Task Create();
+    }
+}
