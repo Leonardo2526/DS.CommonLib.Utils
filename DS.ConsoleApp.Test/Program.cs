9449797af6f7709075d4f28f6d3de6327fa398d8
Данил Сараев <leonardo147wot@gmail.com>
2022-11-20 19:05:47 +0300

    DirectoriesTest added

--- DS.ConsoleApp.Test/Program.cs
+++ DS.ConsoleApp.Test/Program.cs
@@ -13,30 +13,8 @@ namespace DS.ConsoleApp.Test
     {
         [STAThread]
         static void Main(string[] args)
-        {        
-            string defaultDir = "E:\\YandexDisk\\Олимпроект\\";
-            //var specialFolder = Environment.ExpandEnvironmentVariables(defaultDir);
-            var dialog = new FolderBrowserDialog() { SelectedPath = defaultDir };
-            //var dialog = new FolderBrowserDialog() { RootFolder = Environment.SpecialFolder.Desktop };
-            var res = dialog.ShowDialog(); 
-            var sourceDir = res == DialogResult.OK ? dialog.SelectedPath: defaultDir;
-
-            if (sourceDir is null || sourceDir == "") { return; }
-
-            var list = Directory.EnumerateFiles(sourceDir, "*.rvt", SearchOption.AllDirectories).ToList();
-
-            if (!list.Any())
-            {
-                Console.WriteLine("List is 0");
-            }
-            else
-            {
-                foreach (var item in list)
-                {
-                    Console.WriteLine(item);
-                }
-            }
-
+        {
+            DirectoriesTest.Run();
             Console.ReadLine();
             //DialogFormUtils.OpenFormToRead();
         }
