a4caa058a94f18f87a597fbf1e2e15af53dea061
Данил Сараев <leonardo147wot@gmail.com>
2023-01-30 11:17:40 +0300

    DS.WpfApp.Licensing added

---
+++ DS.WpfApp.Licensing/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using DS.WpfApp.Licensing.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DS.WpfApp.Licensing.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        private string _text = "process status";
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; OnPropertyChanged("Text"); }
+        }
+
+        public ICommand Start => new RelayCommand(o =>
+        {
+            MyModel model = new MyModel(this);
+            model.Task1();
+        });
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
