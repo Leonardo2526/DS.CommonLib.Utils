337a0cf07b0e5c42ff87e8ddc716101b41b99536
Данил Сараев <leonardo147wot@gmail.com>
2023-03-10 16:25:25 +0300

    iterator added

---
+++ DS.ClassLib.VarUtils/Iterator.cs
@@ -0,0 +1,24 @@
+﻿namespace DS.ClassLib.VarUtils
+{
+    /// <summary>
+    /// The 'Iterator' abstract class.
+    /// </summary>
+    public abstract class Iterator
+    {
+        public abstract object First();
+        public abstract object Next();
+        public abstract bool IsDone();
+        public abstract object CurrentItem();
+    }
+
+    /// <summary>
+    /// The 'Iterator' generic abstract class.
+    /// </summary>
+    public abstract class Iterator<T>
+    {
+        public abstract T First();
+        public abstract T Next();
+        public abstract bool IsDone();
+        public abstract T CurrentItem();
+    }
+}
