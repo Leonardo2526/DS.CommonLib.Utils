d8da1efd6296b58d9d620807a8f090886a34baec
Данил Сараев <leonardo147wot@gmail.com>
2023-05-29 13:14:46 +0300

    ITransferFactory added

---
+++ DS.ClassLib.VarUtils/ITransferFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace DS.ClassLib.VarUtils
+{
+    /// <summary>
+    /// The interface used to create factories to transfer objects.
+    /// </summary>
+    /// <typeparam name="TSource">Source object to transfer.</typeparam>
+    /// <typeparam name="TTarget">Target place for transfer.</typeparam>
+    public interface ITransferFactory<TSource, TTarget>
+    {
+        /// <summary>
+        /// Transfer <paramref name="source"/> to <paramref name="target"/>.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        /// <returns>Transfered object.
+        /// <para>
+        /// Returns <see langword="null"/> if transfered was failed or <paramref name="source"/> is <see langword="null"/>.
+        /// </para>
+        /// </returns>
+        TSource Transfer(TSource source, TTarget target);
+    }
+
+}
