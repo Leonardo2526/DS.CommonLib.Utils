173c05c6e4d129dbc4404e633f1a034bda9992a2
Danil <leonardo147wot@gmail.com>
2023-11-13 14:28:08 +0300

    LineExt and DS.VarUtils.Test added.

---
+++ DS.ClassLib.VarUtils/LineExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.ClassLib.VarUtils
+{
+    /// <summary>
+    /// Extension methods for <see cref="Line"/>.
+    /// </summary>
+    public static class LineExtensions
+    {
+
+        /// <summary>
+        /// Specifies if <paramref name="point"/> is inside <paramref name="line"/> bounds with specified <paramref name="tolerance"/>.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="point"></param>
+        /// <param name="tolerance"></param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="point"/> lies on <paramref name="line"/> or 
+        /// coinsident with <paramref name="line"/>'s end points.
+        /// <para>
+        /// Otherwise <see langword="false"/>.
+        /// </para>
+        /// </returns>
+        public static bool Contains(this Line line, Point3d point, double tolerance = 0.001)
+        {
+            var dist = line.DistanceTo(point, true);
+            return dist < tolerance;
+        }
+    }
+}
