bb477901a21e0e02740117dc1ee38401ab61ad4c
Danil <leonardo147wot@gmail.com>
2024-02-18 20:56:57 +0300

    AStarGraphAlgorithm added

---
+++ PathFinders/DS.PathFinder/Algorithms/AStar/GraphAlgorithm/Node.cs
@@ -0,0 +1,34 @@
+﻿using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.PathFinder.Algorithms.AStar.GraphAlgorithm
+{
+    public struct Node
+    {
+        public Node(  Point3d location)
+        {
+            Location = location;
+        }
+
+        public Point3d Location { get; }
+
+        /// <summary>
+        /// Path priority.
+        /// </summary>
+        public double F { get; set; }
+
+        /// <summary>
+        /// Distance to parent node.
+        /// </summary>
+        public double G { get; set; }
+
+        /// <summary>
+        /// Distance to end point.
+        /// </summary>
+        public double H { get; set; }
+    }
+}
