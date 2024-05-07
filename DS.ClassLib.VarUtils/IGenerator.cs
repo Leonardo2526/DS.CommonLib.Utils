cbcc9758cc100e3038e4e74893f9edbe1970e023
Danil <leonardo147wot@gmail.com>
2024-05-07 17:48:20 +0300

    Basis operators added

---
+++ DS.ClassLib.VarUtils/IGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace DS.ClassLib.VarUtils
+{
+    /// <summary>
+    /// The interface is used to generate <typeparamref name="T"/> items.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IGenerator<T>
+    {
+        /// <summary>
+        /// Generate a collection of <typeparamref name="T"/>.
+        /// </summary>
+        /// <returns>
+        /// Generate result.
+        /// </returns>
+        IEnumerable<T> Generate();
+    }
+}
